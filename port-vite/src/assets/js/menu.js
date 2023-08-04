75a0d261ec6171d64ab2a8cda5b7afab80bf87f3
wiesanguk <103872528+wiesanguk@users.noreply.github.com>
2023-08-04 17:34:02 +0900

    😇 vite 04

--- port-vite/src/assets/js/menu.js
+++ port-vite/src/assets/js/menu.js
@@ -1,11 +1,13 @@
-const headerToggle = document.getElementById("headerToggle");
-const headerNav = document.querySelector(".header__nav");
+export function menu() {
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header__nav");
 
-if (headerToggle) {
-    headerToggle.addEventListener("click", () => {
-        headerNav.classList.toggle("show");
-        headerToggle.getAttribute("aria-expanded") === "true"
-            ? headerToggle.setAttribute("aria-expanded", "false")
-            : headerToggle.setAttribute("aria-expanded", "true");
-    });
+    if (headerToggle) {
+        headerToggle.addEventListener("click", () => {
+            headerNav.classList.toggle("show");
+            headerToggle.getAttribute("aria-expanded") === "true"
+                ? headerToggle.setAttribute("aria-expanded", "false")
+                : headerToggle.setAttribute("aria-expanded", "true");
+        });
+    }
 }
